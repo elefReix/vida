f33e3597d5173184c9f76a166803701a945534a1
jesus <jesus.elef@gmail.com>
2025-07-02 22:34:16 -0700

    update : navegaciom

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,6 @@ import { ControlComponent } from './gastos/control/control.component';
 export const routes: Routes = [
   { path: '', component: ControlComponent }, // ← HOME (ruta raíz)
   { path: 'home', component: ControlComponent },
-   { path: 'gastos', component: ControlComponent },
+  { path: 'gastos', component: ControlComponent },
   { path: 'gallery', component: CarouselComponent },
 ];
