e06bbc9bfeabaaec8e06717f78a7ee44f59b8105
jesus <jesus.elef@gmail.com>
2025-07-04 22:49:46 -0700

    update : ruta home ordenada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,8 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { ControlComponent } from './gastos/control/control.component';
 
 export const routes: Routes = [
-  { path: '', component: ControlComponent }, // ← HOME (ruta raíz)
+  { path: '', component: LoginComponent },
   { path: 'home', component: ControlComponent },
   { path: 'gastos', component: ControlComponent },
   { path: 'gallery', component: CarouselComponent },
-   { path: '', component: LoginComponent },
 ];
